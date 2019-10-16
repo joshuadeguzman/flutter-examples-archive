e84fe292d25c95a2fcfad908f99648199df548b4
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2019-10-16 19:12:04 +0800

    Update flare setup

--- flutter-add-to-native/flutter_app/lib/embedded_flare_controller/embedded_flare_controller.dart
+++ flutter-add-to-native/flutter_app/lib/embedded_flare_controller/embedded_flare_controller.dart
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+import 'package:flare_dart/math/mat2d.dart';
+import 'package:flare_flutter/flare.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_flutter/flare_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/embedded_flare_controller/flare_controller_channel.dart';
 
@@ -12,21 +16,56 @@ class EmbeddedFlareController extends StatefulWidget {
   }
 }
 
-class EmbeddedFlareControllerState
-    extends State<EmbeddedFlareController> {
-  
+// Animation inspired by https://github.com/2d-inc/Flare-Flutter/blob/stable/example/penguin_dance/lib/main.dart
+class EmbeddedFlareControllerState extends State<EmbeddedFlareController>
+    with FlareController {
+  double _rockAmount = 0.5;
+  double _speed = 1.0;
+  double _rockTime = 0.0;
+  bool _isPaused = false;
+
+  ActorAnimation _rock;
+
   @override
   void initState() {
     super.initState();
     FlareControllerChannelHandler.setChannelHandler(_handleFlareControlChange);
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            FlareActor(
+              "assets/Penguin.flr",
+              alignment: Alignment.center,
+              isPaused: _isPaused,
+              fit: BoxFit.cover,
+              animation: "walk",
+              controller: this,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
-  void _handleFlareControlChange(String aciton) {
+  void _handleFlareControlChange(String aciton) {}
 
+  @override
+  bool advance(FlutterActorArtboard artboard, double elapsed) {
+    _rockTime += elapsed + _speed;
+    _rock.apply(_rockTime % _rock.duration, artboard, _rockAmount);
+    return true;
   }
+
+  @override
+  void initialize(FlutterActorArtboard artboard) {
+    _rock = artboard.getAnimation("music_walk");
+  }
+
+  @override
+  void setViewTransform(Mat2D viewTransform) {}
 }
---
+++ flutter-add-to-native/flutter_app/lib/models/flare_controller_amount.dart
@@ -0,0 +1,22 @@
+// Copyright 2019 Joshua de Guzman (https://joshuadeguzman.github.io). All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+class FlareControllerAmount {
+  int rockAmount;
+  int speed;
+
+  FlareControllerAmount({this.rockAmount, this.speed});
+
+  FlareControllerAmount.fromJson(Map<String, dynamic> json) {
+    rockAmount = json['rockAmount'];
+    speed = json['speed'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['rockAmount'] = this.rockAmount;
+    data['speed'] = this.speed;
+    return data;
+  }
+}
